e98a1ee143de497f382f8f435181e451080253a1
Derek Eom <deom@usc.edu>
2013-02-21 17:01:09 -0800

    Added text removal and destructible platform level (second attempt)

--- CTIN484_FollowYourHeart_Eom_Garcia_10am/Assets/Scripts/UpperVoiceGUI.cs
+++ CTIN484_FollowYourHeart_Eom_Garcia_10am/Assets/Scripts/UpperVoiceGUI.cs
@@ -11,102 +11,102 @@ using System.Collections;
 
 public class UpperVoiceGUI : MonoBehaviour
 {
-    string upperVoiceTransition;
-    public GUIStyle myStyle;
-
-    float nextChar;
-    int charIndex;
-    bool transition;
-
-    public enum states { beginning, appearTransition, disappearTransition, do_nothing };
-    public states myState = states.do_nothing;
-
-    void Start()
-    {
-        upperVoiceTransition = "";
-        //transition = false;
-        myState = states.beginning;
-    }
-
-    void Update()
-    {
-        //if (!transition)
-        // If no text is being processed
-        if (myState == states.beginning)
-        {
-            if (Globals.upperVoice != upperVoiceTransition)								// Check to see if there is new text to process
-            {
-                //transition = true;
-                myState = states.appearTransition;
-                charIndex = 0;
-                nextChar = Time.time + Globals.textSpeed;
-            }
-        }
-        //else
-        if (myState == states.appearTransition)
-        {																				// If text is being processed
-            if (Time.time > nextChar)													// At some time delta, add a new char to text
-            {
-                upperVoiceTransition += Globals.upperVoice[charIndex].ToString();
-                nextChar = Time.time + Globals.textSpeed;
-                charIndex++;
-                if (charIndex >= Globals.upperVoice.Length)
-                {
-                    //transition = false;
-                    myState = states.do_nothing;
-                    Invoke("Dissapear", 2);
-                }
-            }
-        }
-
-        if (myState == states.disappearTransition)
-        {																				// If text is being processed
-            if (Time.time > nextChar)													// At some time delta, delete a char from text
-            {
-                string tempString = upperVoiceTransition;
-                upperVoiceTransition = "";
-                for (int i = 0; i < charIndex; i++)
-                {
-                    upperVoiceTransition += tempString[i];
-                }
-                nextChar = Time.time + Globals.textSpeed;
-                charIndex--;
-                if (charIndex < 0)
-                {
-                    myState = states.do_nothing;
-                }
-            }
-
-            /*
-            if(Time.time > nextChar)													// At some time delta, add a new char to text
-            {
-                Globals.upperVoice.Remove(charIndex,1);
-                Globals.upperVoice.Insert(charIndex,"0");
+	string upperVoiceTransition;
+	public GUIStyle myStyle;
+	
+	float nextChar;
+	int charIndex;
+	bool transition;
+	
+	public enum states {beginning, appearTransition, disappearTransition, do_nothing};
+	public states myState = states.do_nothing;
+	
+	void Start ()
+	{
+		upperVoiceTransition = "";
+		//transition = false;
+		myState = states.beginning;
+	}
+	
+	void Update ()
+	{
+		//if (!transition)
+																		// If no text is being processed
+		if(myState == states.beginning)
+		{
+			if(Globals.upperVoice != upperVoiceTransition)								// Check to see if there is new text to process
+			{
+				//transition = true;
+				myState = states.appearTransition;
+				charIndex = 0;
+				nextChar = Time.time + Globals.textSpeed;
+			}
+		}
+		//else
+		if(myState == states.appearTransition)
+		{																				// If text is being processed
+			if(Time.time > nextChar)													// At some time delta, add a new char to text
+			{
+				upperVoiceTransition += Globals.upperVoice[charIndex].ToString();
+				nextChar = Time.time + Globals.textSpeed;
+				charIndex++;
+				if(charIndex >= Globals.upperVoice.Length)
+				{
+					//transition = false;
+					myState = states.do_nothing;
+					Invoke("Dissapear", 2);
+				}
+			}
+		}
+		
+		if(myState == states.disappearTransition)
+		{																				// If text is being processed
+			if(Time.time > nextChar)													// At some time delta, delete a char from text
+			{
+				string tempString = upperVoiceTransition;
+				upperVoiceTransition = "";
+				for (int i = 0; i < charIndex; i++)
+				{
+					upperVoiceTransition += tempString[i];
+				}
+				nextChar = Time.time + Globals.textSpeed;
+				charIndex--;
+				if(charIndex < 0)
+				{
+					myState = states.do_nothing;
+				}
+			}
+			
+			/*
+			if(Time.time > nextChar)													// At some time delta, add a new char to text
+			{
+				Globals.upperVoice.Remove(charIndex,1);
+				Globals.upperVoice.Insert(charIndex,"0");
 				
-                upperVoiceTransition += Globals.upperVoice[charIndex].ToString();
+				upperVoiceTransition += Globals.upperVoice[charIndex].ToString();
 				
-                nextChar = Time.time + Globals.textSpeed;
-                charIndex++;
-                if(charIndex >= Globals.upperVoice.Length)
-                {
-                    //transition = false;
-                    myState = states.do_nothing;
-                    //Invoke("Dissapear", 2);
-                }
-            }
-            */
-        }
-    }
-
-    void Dissapear()
-    {
-        charIndex = upperVoiceTransition.Length - 1;
-        nextChar = Time.time + Globals.textSpeed;
-        myState = states.disappearTransition;
-    }
-
-    void OnGUI()
-    {
-        GUI.Label(new Rect(10, 10, 100, 20), upperVoiceTransition, myStyle);
-    }
+				nextChar = Time.time + Globals.textSpeed;
+				charIndex++;
+				if(charIndex >= Globals.upperVoice.Length)
+				{
+					//transition = false;
+					myState = states.do_nothing;
+					//Invoke("Dissapear", 2);
+				}
+			}
+			*/
+		}
+	}
+	
+	void Dissapear()
+	{
+		charIndex = upperVoiceTransition.Length - 1;
+		nextChar = Time.time + Globals.textSpeed;
+		myState = states.disappearTransition;
+	}
+	
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 100, 20),upperVoiceTransition , myStyle);
+	}
 }
