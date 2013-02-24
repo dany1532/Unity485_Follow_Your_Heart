cc54756187b4e0061ec621c69f6a4b4735fa0f7c
Daniel <garciada@usc.edu>
2013-02-23 21:47:53 -0800

    Added new level

---
+++ CTIN484_FollowYourHeart_Eom_Garcia_10am/Assets/Scripts/TriggerNumbering.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class TriggerNumbering : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MeshRenderer rnd = this.GetComponent<MeshRenderer>();
+		rnd.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- CTIN484_FollowYourHeart_Eom_Garcia_10am/Assets/Scripts/Triggers.cs
+++ CTIN484_FollowYourHeart_Eom_Garcia_10am/Assets/Scripts/Triggers.cs
@@ -23,5 +23,36 @@ public class Triggers : MonoBehaviour
 		}
 		if(other.gameObject.name == "TempTriggerObject1")
 			Globals.upperVoice = "Goodbye world.";
+		
+		//Story Events
+		
+		if(other.gameObject.name == "StoryTrigger1")
+		{
+			Globals.upperVoice = "I know I can move with A-D Keys...";
+		}
+		
+		if(other.gameObject.name == "StoryTrigger2")
+		{
+			Globals.upperVoice = "I shouldn't have a problem with my SPACE Jump";
+		}
+		
+		if(other.gameObject.name == "StoryTrigger3")
+		{
+			Globals.upperVoice = "As I progress, I begin to wonder...";
+		}
+		if(other.gameObject.name == "StoryTrigger4")
+		{
+			Globals.upperVoice = "Maybe I shouldn't have left...";
+		}
+		
+		if(other.gameObject.name == "StoryTrigger5")
+		{
+			Globals.upperVoice = "What if I don't find anything...";
+			Invoke("lowerVoiceStory5",2);
+		}
+	}
+	
+	void lowerVoiceStory5(){
+		Globals.lowerVoice = "Believe..";	
 	}
 }
